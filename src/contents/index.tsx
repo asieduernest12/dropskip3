4f149dbf11cbb38f6ef71a462e567d01a569b57a
linuxdev <asieduernest@yahoo.co.uk>
2023-09-11 07:00:17 -0400

    Update(mui): works

--- src/contents/index.tsx
+++ src/contents/index.tsx
@@ -1,98 +1,55 @@
 import createCache from "@emotion/cache"
 import { CacheProvider } from "@emotion/react"
 import { Menu as MenuIcon } from "@mui/icons-material"
-import {
-  Box,
-  Button,
-  createTheme,
-  CssBaseline,
-  Paper,
-  Stack
-} from "@mui/material"
-import type { PlasmoCSConfig } from "plasmo"
-import { useEffect, useRef, useState } from "react"
+import { Box, Button, CssBaseline, Paper, Stack } from "@mui/material"
+import ChromaGlass from "data-base64:~assets/Chroma_Glass.png"
+// import CoinSvg from "data-base64:~assets/coin.svg"
+// import CurrencySvg from "data-base64:~assets/currency.svg"
+import type { PlasmoCSConfig, PlasmoGetStyle } from "plasmo"
 
 export const config: PlasmoCSConfig = {
+  // matches: ["*amazon.com*", "*bestbuy.com*", "<all_urls>"],
   matches: ["<all_urls>"],
   world: "MAIN",
   run_at: "document_end"
 }
 
-export default function ShellContainer({ children }) {
-  const ref = useRef<HTMLDivElement>()
-  const [show, setShow] = useState(false)
+const styleElement = document.createElement("style")
 
-  useEffect(() => {
-    setTimeout(() => setShow(true), 2_000)
-  }, [])
+const styleCache = createCache({
+  key: "plasmo-emotion-cache",
+  prepend: true,
+  container: styleElement
+})
+
+export const getStyle: PlasmoGetStyle = () => styleElement
+
+export default function ContentShell() {
+  console.log("ContentShell.fired")
 
   return (
-    <Stack direction="column" ref={ref} className="shellContainer">
-      <Button>dropskip</Button>
-      {ref?.current ? <Content reference={ref} /> : <Button>Loading</Button>}
-    </Stack>
-  )
-}
-
-function Content({
-  reference
-}: {
-  reference: ReturnType<typeof useRef<HTMLDivElement>>
-}) {
-  console.log("dropskipped content fired")
-  // const container = document.querySelector("#shadow-root")
-  console.log(reference)
-  const container = reference?.current
-
-  if (!container) {
-    return <Button>No container:#shadow root</Button>
-  }
-
-  // const shadowContainer = container.attachShadow({ mode: "open" })
-  const shadowContainer = container.parentNode
-  const emotionRoot = document.createElement("style")
-  const shadowRootElement = document.createElement("div")
-  shadowContainer.appendChild(emotionRoot)
-  shadowContainer.appendChild(shadowRootElement)
-
-  const cache = createCache({
-    key: "css",
-    prepend: true,
-    container: emotionRoot
-  })
-
-  const theme = createTheme({
-    components: {
-      MuiPopover: {
-        defaultProps: {
-          container: shadowRootElement
-        }
-      },
-      MuiPopper: {
-        defaultProps: {
-          container: shadowRootElement
-        }
-      },
-      MuiModal: {
-        defaultProps: {
-          container: shadowRootElement
-        }
-      }
-    }
-  })
-  return (
-    <Box>
-      <CacheProvider value={cache}>
-        <CssBaseline />
-        <Paper>
-          <Stack direction="column" sx={{ padding: 2, gap: 2 }}>
-            <Button variant="contained" startIcon={<MenuIcon />}>
-              view product only
-            </Button>
-            <Button>Checkout for 30% ($14.77) off</Button>
-          </Stack>
-        </Paper>
-      </CacheProvider>
-    </Box>
+    <CacheProvider value={styleCache}>
+      <CssBaseline />
+      <Paper
+        className="contentShell"
+        sx={{ bottom: 0, left: 0, position: "fixed", margin: 2 }}
+        elevation={2}>
+        <Stack direction="column" sx={{ padding: 2, gap: 2 }}>
+          <Box>
+            <strong>Match Found</strong>
+          </Box>
+          <Button variant="contained" startIcon={<MenuIcon />}>
+            view product only
+          </Button>
+          <Button
+            sx={{
+              background: `url(${ChromaGlass}) -50px 0px`,
+              color: "white"
+            }}>
+            <strong>Checkout for 30% ($14.77) off</strong>
+          </Button>
+        </Stack>
+      </Paper>
+    </CacheProvider>
   )
 }
